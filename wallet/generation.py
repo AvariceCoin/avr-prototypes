81d5258cb31982dc765fc7a8663b139b8dcaf538
ISTEK <tamdacoder@gmail.com>
2025-09-29 09:54:50 -0500

    Create generation.py

---
+++ wallet/generation.py
@@ -0,0 +1,40 @@
+from bip32utils import BIP32Key
+from Crypto.Hash import RIPEMD
+from termcolor import colored
+from mnemonic import Mnemonic
+from hashlib import sha256
+import base58
+import os
+
+globj = Mnemonic("english")
+
+wallet = ""
+seedphrase = ""
+public = b""
+private = b""
+
+def wallet():
+  global wallet, seedphrase, public, private
+  entropy = os.urandom(32)
+  seedphrase = globj.to_menomic(entropy)
+  seed = globj.to_seed(seedphrase, passphrase="")
+
+  master = BIP32Key.fromEntropy(seed)
+  private = master.PrivateKey()
+  public = master.PublicKey()
+
+  version = b"0x54"
+  pubhash = RIPEMD.new(public)
+  checksum = sha256(sha256(version + pubhash).digest()).digest()[:4]
+  raw = base58.b58encode(version + pubhash + checksum).decode()
+
+  extract = base58.b58encode(raw)
+  ev = extract[0:1]
+  eph = extract[1:21]
+  ec = extract[21:25]
+  if sha256(sha256(ev + eph + ec).digest()).digest()[:4] == checksum:
+    wallet = "G" + raw + "RD"
+  else:
+    wallet = "INVALID"
+
+wallet()
