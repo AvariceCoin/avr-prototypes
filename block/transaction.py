de8b9dd06c0e3d69433cb7e7aa69e9000de4f98d
ISTEK <tamdacoder@gmail.com>
2025-09-29 14:20:01 -0500

    Create transaction.py

---
+++ block/transaction.py
@@ -0,0 +1,58 @@
+from bip32utils import BIP32Key
+from Crypto.Hash import RIPEMD
+from termcolor import colored
+from mnemonic import Mnemonic
+from hashlib import sha256
+import random
+import base58
+import ecdsa
+import time
+import os
+
+globj = Mnemonic("english")
+
+master = ""
+wallet = ""
+seedphrase = ""
+public = b""
+private = b""
+
+def wallet():
+  global wallet, seedphrase, public, private, master
+  entropy = os.urandom(32)
+  seedphrase = globj.to_mnemonic(entropy)
+  seed = globj.to_seed(seedphrase, passphrase="")
+
+  master = BIP32Key.fromEntropy(seed)
+  private = master.PrivateKey()
+  public = master.PublicKey()
+
+  version = b"0x54"
+  pubhash = RIPEMD.new(public).digest()
+  checksum = sha256(sha256(version + pubhash).digest()).digest()[:4]
+  raw = base58.b58encode(version + pubhash + checksum).decode()
+
+  extract = base58.b58encode(raw)
+  ev = extract[0:1]
+  eph = extract[1:21]
+  ec = extract[21:25]
+  if sha256(sha256(ev + eph + ec).digest()).digest()[:4] == checksum:
+    wallet = "G" + raw + "RD"
+  else:
+    wallet = "INVALID"
+
+wallet()
+signings = ecdsa.SigningKey.from_string(private, curve=ecdsa.SECP256K1)
+
+def transaction():
+  fromWallet = wallet
+  wallet()
+  toWallet = wallet
+  timestamp = str(time.time())
+  amount = random.randint(1, 10000)
+  fee = amount * (random.randint(90, 99) / 100)
+  sig = signings.sign_digest(fromWallet.encode("utf-8") + toWallet.encode("utf-8") + timestamp.encode("utf-8") + amount.to_bytes(4, "big") + fee.to_bytes(4, "big"))
+  txid = sha256(fromWallet.encode("utf-8") + toWallet.encode("utf-8") + timestamp.encode("utf-8") + amount.to_bytes(4, "big") + fee.to_bytes(4, "big") + signature).hexdigest()
+  return = {"from": fromWallet, "to": toWallet, "timestamp": timestamp, "amount": amount, "fee": fee, "signature": sig.hex(), "txid": txid}
+
+print(transaction())
